99812335756439850c6a4a536adf68e0548cd017
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2011-08-06 22:51:04 -0400

    Add wrappers for Platform functions
    These are on the exports.Geo method

--- Resources/lib/atlas.js
+++ Resources/lib/atlas.js
@@ -17,6 +17,152 @@ function deg2rad (angle) {
    return (angle / 180) * Math.PI;
 };
 
+function translateErrorCode(code) {
+	if (code === null) {
+		return 'Unknown';
+	}
+	switch (code) {
+		case Ti.Geolocation.ERROR_LOCATION_UNKNOWN:
+			return "Location unknown";
+		case Ti.Geolocation.ERROR_DENIED:
+			return "Access denied";
+		case Ti.Geolocation.ERROR_NETWORK:
+			return "Network error";
+		case Ti.Geolocation.ERROR_HEADING_FAILURE:
+			return "Failure to detect heading";
+		case Ti.Geolocation.ERROR_REGION_MONITORING_DENIED:
+			return "Region monitoring access denied";
+		case Ti.Geolocation.ERROR_REGION_MONITORING_FAILURE:
+			return "Region monitoring access failure";
+		case Ti.Geolocation.ERROR_REGION_MONITORING_DELAYED:
+			return "Region monitoring setup delayed";
+		default:
+			return 'Unknown';
+	}
+};
+
+//These are the comment device geo methods
+var _geo = {
+	
+	enabled : function(){
+		
+		if(!Ti.Geolocation.locationServicesEnabled){
+			return false;
+		}
+		
+		//If we're on iOS
+		if (Titanium.Platform.name !== 'android') {
+			var authorization = Titanium.Geolocation.locationServicesAuthorization;
+			if (authorization == Titanium.Geolocation.AUTHORIZATION_DENIED) {
+				return false;
+			}
+			else if (authorization == Titanium.Geolocation.AUTHORIZATION_RESTRICTED) {
+				return false;
+			}			
+		}
+		
+		return true;
+	},
+	//Wrapper around Ti functions to get Heading
+	getCurrentHeading : function(callback){
+		var results = {success:false};
+		Ti.Geolocation.getCurrentHeading(function(e){
+			if (!e.success || e.error){
+				results.success=false;
+				results.message=translateErrorCode(e.code);
+				if((callback!==null)&&(callback!==undefined)){
+					callback(results);	
+				}
+				return;
+			}
+			results.success=true;	
+			results.x = e.heading.x;
+			results.y = e.heading.y;
+			results.z = e.heading.z;
+			results.magneticHeading = e.heading.magneticHeading;
+			results.accuracy = e.heading.accuracy;
+			results.trueHeading = e.heading.trueHeading;
+			results.timestamp = e.heading.timestamp;
+			if((callback!==null)&&(callback!==undefined)){
+				callback(results);	
+			}
+		});		
+	},
+	//Wrapper around Ti functions to get GPS coordinates
+	getCurrentCoordinates : function(callback){
+		var results = {success:false};
+		Ti.Geolocation.getCurrentPosition(function(e){
+			if (!e.success || e.error){
+				results.success=false;
+				results.message=translateErrorCode(e.code);
+				if((callback!==null)&&(callback!==undefined)){
+					callback(results);	
+				}
+				return;
+			}
+			
+			results.success=true;			
+			results.longitude = e.coords.longitude;
+			results.latitude = e.coords.latitude;
+			results.altitude = e.coords.altitude;
+			results.heading = e.coords.heading;
+			results.accuracy = e.coords.accuracy;
+			results.speed = e.coords.speed;
+			results.timestamp = e.coords.timestamp;
+			results.altitudeAccuracy = e.coords.altitudeAccuracy;
+			if((callback!==null)&&(callback!==undefined)){
+				callback(results);	
+			}			
+		
+		});		
+	},
+	//Method to setup all of the Ti.Geolocation values
+	setup : function(setupDetails){
+		if((setupDetails.purpose!==undefined)&&(setupDetails.purpose!==undefined)){
+			Ti.Geolocation.purpose=setupDetails.purpose;
+		}
+		if((setupDetails.accuracy!==undefined)&&(setupDetails.accuracy!==undefined)){
+			Ti.Geolocation.accuracy=setupDetails.accuracy;
+		}	
+		if((setupDetails.preferredProvider!==undefined)&&(setupDetails.preferredProvider!==undefined)){
+			Ti.Geolocation.preferredProvider=setupDetails.preferredProvider;
+		}
+		if((setupDetails.headingFilter!==undefined)&&(setupDetails.headingFilter!==undefined)){
+			Ti.Geolocation.headingFilter=setupDetails.headingFilter;
+		}
+		if((setupDetails.distanceFilter!==undefined)&&(setupDetails.distanceFilter!==undefined)){
+			Ti.Geolocation.distanceFilter=setupDetails.distanceFilter;
+		}
+	},
+	setSccuracy : function(accuracy){
+		// SET ACCURACY - THE FOLLOWING VALUES ARE SUPPORTED
+		// Titanium.Geolocation.ACCURACY_BEST
+		// Titanium.Geolocation.ACCURACY_NEAREST_TEN_METERS
+		// Titanium.Geolocation.ACCURACY_HUNDRED_METERS
+		// Titanium.Geolocation.ACCURACY_KILOMETER
+		// Titanium.Geolocation.ACCURACY_THREE_KILOMETERS		
+		Ti.Geolocation.accuracy = accuracy;
+	},
+	setPreferredProvider : function(preferredProvider){
+		Ti.Geolocation.preferredProvider = preferredProvider;
+	},
+	setPurpose : function(purpose){
+		Ti.Geolocation.purpose = purpose;
+	}
+};
+
+//If geo is enabled try to set some defaults
+if(_geo.enabled()){
+	Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+	Ti.Geolocation.preferredProvider = "gps";
+	Ti.Geolocation.purpose = "GPS access for App usage";
+	Ti.Geolocation.headingFilter = 90;
+	Ti.Geolocation.distanceFilter = 10;
+	if (Ti.Geolocation.hasCompass){
+		//  TURN OFF ANNOYING COMPASS INTERFERENCE MESSAGE
+		Ti.Geolocation.showCalibration = false;
+	}
+}
 
 var _math = {
 	
@@ -117,3 +263,4 @@ var _shapes = {
 //Set the functions so they are available via exports
 exports.Math = _math;
 exports.Shapes = _shapes;
+exports.Geo = _geo;
